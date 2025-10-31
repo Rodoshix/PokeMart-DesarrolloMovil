0889fcc6cc12a75648b0d9c423e5a109d51146f1
Barellanog <benj.arellano@duocuc.cl>
2025-10-30 23:55:29 -0300

    Estructura del Proyecto
    Se crea el esqueleto del proyecto.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,11 +5,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.pokemart"
+    namespace = "com.pokermart.ecommerce"
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.example.pokemart"
+        applicationId = "com.pokermart.ecommerce"
         minSdk = 24
         targetSdk = 36
         versionCode = 1
@@ -27,20 +27,22 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
     kotlinOptions {
         jvmTarget = "11"
     }
+
     buildFeatures {
         compose = true
     }
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -49,11 +51,14 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+
     testImplementation(libs.junit)
+
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
 }
--- app/src/main/java/com/pokermart/ecommerce/ui/theme/Color.kt
+++ app/src/main/java/com/pokermart/ecommerce/ui/theme/Color.kt
@@ -1,4 +1,4 @@
-package com.example.pokemart.ui.theme
+package com.pokermart.ecommerce.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
